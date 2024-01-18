995df38f8c38a965c654c0bbb97141e648752cdb
Shamsudin <shamsudinov93@bk.ru>
2024-01-18 11:12:01 +0300

    Разработка
    Реализовал метод для удаления элемента из конца вектора.
    так же метод для проверки пустоты вектора.
    определил тип беззнакового целого для работы с вектором.

--- Vector/vector.cpp
+++ Vector/vector.cpp
@@ -2,21 +2,33 @@
 
 Vector::Vector(){
 
-    memory_capacity = 100;
+    memory_capacity = 10;
     curr_pos = 0;
     data = new int[memory_capacity];
+    init(0);
+    std::cout << "Vector()" << std::endl;
 }
 
-Vector::Vector(int size){
+Vector::Vector(vtype size){
 
     this->memory_capacity = size;
     curr_pos = 0;
     data = new int[this->memory_capacity];
+    init(0);
+    std::cout << "Vector(int size)" << std::endl;
 }
 
 Vector::~Vector(){
 
     delete [] data;
+    std::cout << "~Vector()" << std::endl;
+}
+
+void Vector::init(int val){
+
+    for(unsigned int index = 0; index < memory_capacity; index++){
+        data[index] = val;
+    }
 }
 
 void Vector::push_back(int val){
@@ -33,23 +45,34 @@ void Vector::push_back(int val){
 
 void Vector::pop_back(){
 
+    if(empty()){
+        return;
+    }
 
+    data[curr_pos] = 0;
+    curr_pos--;
+    curr_size = curr_pos;
 }
 
-int Vector::size() const{
+bool Vector::empty(){
+
+    return (curr_size == 0) ? true : false;
+}
+
+vtype Vector::size() const{
 
     return curr_size;
 }
 
-int Vector::capacity() const{
+vtype Vector::capacity() const{
 
     return memory_capacity;
 }
 
 void Vector::print() const{
 
-    for(int i=0; i<curr_size; i++){
-        std::cout << data[i] << " ";
+    for(unsigned int index=0; index<curr_size; index++){
+        std::cout << data[index] << " ";
     }
     std::cout<<std::endl;
 }
--- Vector/vector.h
+++ Vector/vector.h
@@ -1,11 +1,31 @@
 #ifndef VECTOR_H
 #define VECTOR_H
 
+#include <iostream>
+
+typedef unsigned int vtype;
 
 class Vector
 {
+    vtype memory_capacity;
+    int *data = nullptr;
+    vtype curr_pos;
+    vtype curr_size;
 public:
     Vector();
+    Vector(vtype size);
+    ~Vector();
+
+    void init(int val);
+
+    void push_back(int val);
+    void pop_back();
+
+    bool empty();
+    vtype size() const;
+    vtype capacity() const;
+
+    void print() const;
 };
 
 #endif // VECTOR_H
