02c8196e33ad8af7a58db053ac6f03b3f37f9fad
Shamsudin <shamsudinov93@bk.ru>
2024-01-18 09:44:40 +0300

    Начало работы
    Реализовал базовые методы для работы с вектором.
    -добавление элемента
    - конструктор и деструктор
    - получение размера
    - вывод элементов

--- Vector/vector.cpp
+++ Vector/vector.cpp
@@ -1,6 +1,55 @@
 #include "vector.h"
 
-Vector::Vector()
-{
+Vector::Vector(){
 
+    memory_capacity = 100;
+    curr_pos = 0;
+    data = new int[memory_capacity];
+}
+
+Vector::Vector(int size){
+
+    this->memory_capacity = size;
+    curr_pos = 0;
+    data = new int[this->memory_capacity];
+}
+
+Vector::~Vector(){
+
+    delete [] data;
+}
+
+void Vector::push_back(int val){
+
+    if(curr_size == memory_capacity){
+        // realloc
+        return;
+    }
+
+    data[curr_pos] = val;
+    curr_pos++;
+    curr_size = curr_pos;
+}
+
+void Vector::pop_back(){
+
+
+}
+
+int Vector::size() const{
+
+    return curr_size;
+}
+
+int Vector::capacity() const{
+
+    return memory_capacity;
+}
+
+void Vector::print() const{
+
+    for(int i=0; i<curr_size; i++){
+        std::cout << data[i] << " ";
+    }
+    std::cout<<std::endl;
 }
